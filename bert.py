71fb86487a3287b947e3ddfcd93a09f9b2eb5147
imeansu <gcnml10@naver.com>
2021-11-17 09:48:34 +0900

    [feat] papago translate

--- bert.py
+++ bert.py
@@ -3,6 +3,9 @@ from topicMessage import TopicResponseMessage
 from publisher import Publisher
 from BERT_Arch import BERT_Arch2
 from BERT_Arch import Run_BERT
+from langdetect import detect
+import naverClient
+import requests
 
 class Bert():
 
@@ -10,6 +13,7 @@ class Bert():
         self.publisher = Publisher("my_queue")
         self.publisher.initPublish("topic", host='redis.slss29.ng.0001.apn2.cache.amazonaws.com', port=6379, db=0)
         self.runBert = Run_BERT()
+        self.naverClient = naverClient()
         
 #'52.78.117.179'
     def generateTopic(self, topicRequestMessage):
@@ -17,8 +21,30 @@ class Bert():
         추천 로직~~~
 
         """
+        sentences = topicRequestMessage.sentences
+        for idx, sen in enumerate(sentences):
+            if detect(sen) == "ko":
+                sentences[idx] = self.papago(sen)
         text = " ".join(topicRequestMessage.sentences)
         result = self.runBert.run(text)
         topicResponseMessage = TopicResponseMessage(topicRequestMessage.members, result)
         self.publisher.publish(topicResponseMessage)
         return 
+
+    def papago(self, text):
+        base_url = "https://openapi.naver.com/v1/papago/n2mt"
+        headers = {"X-Naver-Client-id": naverClient.ClientID, "X-Naver-Client-Secret": naverClient.ClientSecret}
+        data = {'text' : text, 'source' : 'ko', 'target' : 'en'}
+        response = requests.post(base_url, headers=headers, data=data)
+        rescode = response.status_code
+
+        if rescode == 200:
+            send_data = response.json()
+            trans_data = send_data['message']['result']['translatedText']
+            print(f"trans_Data : {trans_data}")
+            return trans_data
+        else:
+            print("Error Code: ", rescode)
+            return " "
+
+
