667633fb6de580a93365b1f13ff4e71a4290f5de
imeansu <gcnml10@naver.com>
2021-11-17 10:46:27 +0900

    [fix] tmp

--- BERT_Arch.py
+++ BERT_Arch.py
@@ -25,7 +25,7 @@ class BERT_Arch2(nn.Module):
 
 class Run_BERT:
     def __init__(self):
-        self.tmp = ['bts', 'blackpink', 'netflix', 'nct', 'kpop', 'squid_game', 'twice', 'north_korea', 'itzy']
+        self.tmp = ['bts', 'blackpink', 'itzy', 'nct', 'exo', 'twice', 'netflix', 'squid_game', 'kpop', 'twice', 'korean_food', 'north_korea']
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
         self.device = torch.device("cpu")
         self.model = torch.load('./model_v1.pt', map_location=self.device)
